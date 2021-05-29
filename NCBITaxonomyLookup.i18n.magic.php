41173635de0c14582eab5c8295a0fd6830fa7b6f
zoranzoki21 <zorandori4444@gmail.com>
2021-05-30 01:55:01 +0200

    Add alias for Serbian langugage
    Change-Id: Ib6704077845fc193ebc7e455e6010de2938b4b3c

--- NCBITaxonomyLookup.i18n.magic.php
+++ NCBITaxonomyLookup.i18n.magic.php
@@ -5,9 +5,29 @@
  */
 $magicWords = [];
 
-/** English
+/**
+ * English
+ *
  * @author WikiTeq
  */
 $magicWords['en'] = [
 	'taxonomy' => [ 0, 'taxonomy' ]
+];
+
+/**
+ * Serbian Cyrillic (српски (ћирилица))
+ *
+ * @author Zoran Dori (WikiTeq)
+ */
+$magicWords['sr-ec'] = [
+	'taxonomy' => [ 0, 'таксономија' ]
+];
+
+/**
+ * Serbian Latin (srpski (latinica))
+ *
+ * @author Zoran Dori (WikiTeq)
+ */
+$magicWords['sr-el'] = [
+	'taxonomy' => [ 0, 'taksonomija' ]
 ];
